f8daf0cb130c07f4269866c9be43611bcbb00a44
Cloudox <cloudox@foxmail.com>
2018-07-11 10:46:07 +0800

    Initial Commit

---
+++ LSCTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSCTextDemo
+//
+//  Created by csdc-iMac on 2018/7/11.
+//  Copyright © 2018年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LSCTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSCTextDemo
+//
+//  Created by csdc-iMac on 2018/7/11.
+//  Copyright © 2018年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
